58b26bdb8c553dc6c34c91e622e5c39f0a4e4884
Conitooo <pjcordova@iescotarelo.org>
2024-11-22 21:26:35 +0100

    Mejora en los inputs

--- src/Ventana/Lamina.java
+++ src/Ventana/Lamina.java
@@ -2,13 +2,10 @@ package Ventana;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-
+import java.awt.event.*;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -34,69 +31,70 @@ public class Lamina extends JPanel implements ActionListener, KeyListener{
         setLayout(new BorderLayout()); //Nuevo layout para poder organizar los elementos.
         texto = new JTextField();
         requestFocus();
-        texto.addKeyListener(this);
+        texto.setEditable(false);
 
         texto.setHorizontalAlignment(SwingConstants.RIGHT); //Dicta la escritura de derecha a izquierda.
         texto.setFont(new Font("Arial", Font.BOLD, 50));
 
         add(texto, BorderLayout.NORTH); //Coloca el marco de texto en la parte superior de la lámina.
-
         JPanel panelbotones = new JPanel(); //Creamos un nuevo panel para colocar los botones.
-        panelbotones.setLayout(new GridLayout(4, 4, 2, 2)); //Creamos un GridLayout dentro del Panel para organizar los botones.
+        panelbotones.setLayout(new GridLayout(4, 6, 2, 2)); //Creamos un GridLayout dentro del Panel para organizar los botones.
         agregarBotones(botones, panelbotones);
 
         add(panelbotones, BorderLayout.CENTER); //Agregamos el panel a la lámina en el centro. Si lo colocamos así, ocupa todo el tamaño restante.
-
+        texto.addKeyListener(this);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
-        
-        String comando = e.getActionCommand(); //Obtiene el texto que contiene los botones.
-        if ("0123456789".contains(comando)) {
-            texto.setEditable(true);
-            texto.setText(texto.getText() + comando);//Agrega el texto al textField.
-            requestFocus();
-        } else if ("+-/*".contains(comando)) {
-            operador = comando;
-            num1 = Double.parseDouble(texto.getText());
-            texto.setText(" ");
-            requestFocus();
-        } else if (comando.equals("=")) {
-            num2 = Double.parseDouble(texto.getText());
-            requestFocus();
-            //Switch para poder gestionar los signos.
-            
-            switch (operador) {
-                case "+":
-                    texto.setText(String.valueOf(Operaciones.suma(num1, num2)));
+        try {
+            String comando = e.getActionCommand(); //Obtiene el texto que contiene los botones.
+            if ("0123456789".contains(comando)) {   
+                texto.setText(texto.getText() + comando);//Agrega el texto al textField.
+            } else if ("+-/*".contains(comando)) {
+                operador = comando;
+                num1 = Double.parseDouble(texto.getText());
+                texto.setText(" ");
 
-                    break;
-                case "-":
-                    texto.setText(String.valueOf(Operaciones.resta(num1, num2)));
+            } else if (comando.equals("=")) {
+                num2 = Double.parseDouble(texto.getText());
 
-                    break;
-                case "*":
-                    texto.setText(String.valueOf(Operaciones.producto(num1, num2)));
-
-                    break;
-
-                case "/":
-                    texto.setText(String.valueOf(Operaciones.div(num1, num2)));
-
-                    break;
-
-                default:
-                    break;
+                //Switch para poder gestionar los signos.
+                
+                switch (operador) {
+                    case "+":
+                        texto.setText(String.valueOf(Operaciones.suma(num1, num2)));
+    
+                        break;
+                    case "-":
+                        texto.setText(String.valueOf(Operaciones.resta(num1, num2)));
+    
+                        break;
+                    case "*":
+                        texto.setText(String.valueOf(Operaciones.producto(num1, num2)));
+    
+                        break;
+    
+                    case "/":
+                        texto.setText(String.valueOf(Operaciones.div(num1, num2)));
+    
+                        break;
+    
+                    default:
+                        break;
+                }
             }
+            else if(comando.equals("C")){
+                texto.setText("");
+                num1 = 0;
+                num2=0;
+            }
+    
+        } catch (NullPointerException | NumberFormatException j) {
+            j.printStackTrace();
         }
-        else if(comando.equals("C")){
-            texto.setText("");
-            num1 = 0;
-            num2=0;
-        }
-
+       
     }
 
     /**
@@ -109,31 +107,71 @@ public class Lamina extends JPanel implements ActionListener, KeyListener{
             String boton = botones[i] + "";
             JButton btn = new JButton(boton);
             panel.add(btn);
-
+            btn.setFocusable(false);
             btn.setFont(new Font("Arial", Font.BOLD, 20));
             btn.setBackground(Color.GRAY);
             btn.setForeground(Color.WHITE);
-
+            btn.setFocusable(false);
             btn.addActionListener(this);
         }
     }
 
+    @Override
     public void keyPressed(KeyEvent e) {
-        
-        if(e.getKeyCode()==KeyEvent.VK_NUMPAD2){
-            texto.setText("Hola");
+        if(e.getKeyLocation()==KeyEvent.KEY_LOCATION_NUMPAD){
+            String comando = e.getKeyChar()+""; //Obtiene el texto que contiene los botones.
+            if ("0123456789".contains(comando)) {
+                texto.setText(texto.getText() + comando);//Agrega el texto al textField.
+
+            } else if ("+-/*".contains(comando)) {
+                operador = comando;
+                num1 = Double.parseDouble(texto.getText());
+                texto.setText(" ");
+            
+
+            } else if (e.getKeyCode()==10) {
+                num2 = Double.parseDouble(texto.getText());
+                //Switch para poder gestionar los signos.
+                
+                switch (operador) {
+                    case "+":
+                        texto.setText(String.valueOf(Operaciones.suma(num1, num2)));
+                        break;
+                    case "-":
+                        texto.setText(String.valueOf(Operaciones.resta(num1, num2)));
+                        break;
+                    case "*":
+                        texto.setText(String.valueOf(Operaciones.producto(num1, num2)));
+                        break;
+                    case "/":
+                        texto.setText(String.valueOf(Operaciones.div(num1, num2)));
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else if(comando.equals("C")){
+                texto.setText("");
+                num1 = 0;
+                num2=0;
+            }
+            
+
         }
-        
+      
         
     }
     @Override
     public void keyReleased(KeyEvent e) {
-        System.out.println("goooo");
+        
         
     }
     @Override
     public void keyTyped(KeyEvent e) {
-        // TODO Auto-generated method stub
+        
         
     }
+
+    
+    
 }
